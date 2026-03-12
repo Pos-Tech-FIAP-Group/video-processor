4915d0abc63fa159a732f71a955b86090bb7b2fe
VitorBrazSilva <vitorbraz_@hotmail.com>
2026-03-11 22:33:52 -0300

    remoção de import invalid

--- video-service/src/main/java/com/fiap/fiapx/video/adapters/driver/api/controller/VideoController.java
+++ video-service/src/main/java/com/fiap/fiapx/video/adapters/driver/api/controller/VideoController.java
@@ -11,18 +11,11 @@ import com.fiap.fiapx.video.core.application.usecases.ListVideosByUserUseCase;
 import com.fiap.fiapx.video.core.application.usecases.command.CreateVideoCommand;
 import com.fiap.fiapx.video.core.domain.model.Video;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.UUID;
 
 @RestController
